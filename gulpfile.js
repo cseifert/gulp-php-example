f4f586c800d159a99f5f799a9743559e38f15d02
Christian Seifert <christian-f-seifert@gmx.de>
2016-05-08 13:53:58 +0200

    [TASK] Add PHPMetrics

--- gulpfile.js
+++ gulpfile.js
@@ -13,10 +13,16 @@ var config = {
     pdependDocDir: 'docs/pdepend'
 };
 
+/**
+ * Checks for PHP syntax errors
+ */
 gulp.task('phplint', function () {
     return phplint([config.phpDir + '/**/*.php'], {limit: 10}, function(){});
 });
 
+/**
+ * Runs Code Sniffer over the PHP files
+ */
 gulp.task('phpcs', function () {
     return gulp.src(config.phpDir + '/**/*.php')
         .pipe(phpcs({
@@ -27,31 +33,66 @@ gulp.task('phpcs', function () {
         .pipe(phpcs.reporter('log'));
 });
 
+/**
+ * Beatifies the code according to PSR-2
+ */
 gulp.task('phpcbf', shell.task([config.vendorBin + 'phpcbf --standard=PSR2 ' + config.phpDir]));
 
-gulp.task('phpmd', shell.task([config.vendorBin + 'phpmd ' + config.phpDir + ' html codesize,unusedcode,naming,design,cleancode,controversial --reportfile ' + config.docDir + '/phpmd.html --suffixes ' + config.phpDir]));
+/**
+ * Runs PHP mess detector
+ */
+gulp.task('phpmd', shell.task([
+	'mkdir -p ' + config.docDir,
+	config.vendorBin + 'phpmd ' + config.phpDir + ' html codesize,unusedcode,naming,design,cleancode,controversial --reportfile ' + config.docDir + '/phpmd.html --suffixes ' + config.phpDir
+]));
 
+/**
+ * Runs pdepend to gain information about metrics
+ */
 gulp.task('pdepend', shell.task([
     'mkdir -p ' + config.pdependDocDir,
     config.vendorBin + 'pdepend --summary-xml=' + config.pdependDocDir + '/summary.xml --jdepend-chart=' + config.pdependDocDir + '/chart.svg --overview-pyramid=' + config.pdependDocDir + '/pyramid.svg --suffix=php ' + config.phpDir
 ]));
 
+/**
+ * Finds duplicate code
+ */
 gulp.task('phpcpd', shell.task([config.vendorBin + 'phpcpd ' + config.phpDir]));
 
+/**
+ * Generates a metrics report as HTML file
+ */
+gulp.task('phpmetrics', shell.task([
+	'mkdir -p ' + config.docDir,
+	config.vendorBin + 'phpmetrics --report-html=' + config.docDir + '/metrics.html ' + config.phpDir
+]));
+
+/**
+ * Runs PHPUnit to execute unit tests
+ */
 gulp.task('phpunit', function() {
     return gulp.src('phpunit.xml')
         .pipe(phpunit(config.vendorBin + 'phpunit'))
         .on('error', function(){console.error('The tests failed')});
 });
 
+/**
+ * Runs an API generator
+ */
 gulp.task('phpdoc', shell.task([config.vendorBin + 'phpdoc -d ' + config.phpDir + ' -t ' + config.phpdocDir]));
 
+/**
+ * Watches PHP files for changes and runs according tasks
+ */
 gulp.task('watch', function () {
     gulp.watch([config.phpDir + '/**/*.php'], function(){
         runSequence('phplint', 'phpcs', 'phpunit', 'phpmd', 'phpcpd')
     });
 });
 
-gulp.task('default', function() {
-    runSequence('phplint', 'phpcs', 'phpcbf', 'phpunit', 'phpmd', 'phpcpd');
+/**
+ * Runs a selection of tasks
+ */
+gulp.task('default', function(callback) {
+    runSequence('phplint', 'phpcs', 'phpcbf', 'phpunit', 'phpmd', 'phpcpd', callback);
 });
